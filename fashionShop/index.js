3543f7a05d777b345ff8de2e1df179b2c41b3cc4
Angelica Libang <libangangelicah@gmail.com>
2024-01-22 21:06:52 +0800

    update img folder, add index.html, update fashionShop folder, update css folder

--- fashionShop/index.js
+++ fashionShop/index.js
@@ -3,56 +3,67 @@ const ps = require("./shop");
 const cors = require("cors");
 
 const app = ex();
-const s = ps.shop;
+const shopData = ps.shop;
 
 app.use(ex.json());
 app.use(cors());
 
 // api
 app.get("/api/shop/", (req, res) => {
-  res.send(s);
+  res.send(shopData);
 });
 
 app.get("/api/shop/:id", (req, res) => {
-  let fashion = false;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i].id == Number(req.params.id)) {
-      fashion = s[i];
-      break;
+  const itemId = Number(req.params.id);
+
+  for (let i = 0; i < shopData.length; i++) {
+    const category = shopData[i];
+    const item = category.items.find((item) =>
+      item.classification.some((c) => c.id === itemId)
+    );
+
+    if (item) {
+      res.send(item);
+      console.log(item);
+      return;
     }
   }
-  if (fashion) {
-    res.send(fashion);
-    console.log(fashion);
-  } else {
-    let err = "Item not found!";
-    res.status(404);
-    res.send(err);
-    console.error(err);
-  }
+
+  let err = "Item not found!";
+  res.status(404);
+  res.send(err);
+  console.error(err);
 });
 
-app.get("/api/shop/items/classification/image", (req, res) => {
-  let image = [];
-  for (let i = 0; i < s.length; i++) {
-    if (image.includes(s[i].image) == false) {
-      image.push(s[i].image);
+app.get("/api/shop/items/:category/:type/:classification", (req, res) => {
+  const { category, type, classification } = req.params;
+
+  for (let i = 0; i < shopData.length; i++) {
+    const categoryObj = shopData[i];
+
+    if (categoryObj.category.toLowerCase() === category.toLowerCase()) {
+      for (let j = 0; j < categoryObj.items.length; j++) {
+        const item = categoryObj.items[j];
+
+        if (item.type.toLowerCase() === type.toLowerCase()) {
+          const matchingClassifications = item.classification.filter(
+            (c) =>
+              c.classification.toLowerCase() === classification.toLowerCase()
+          );
+
+          if (matchingClassifications.length > 0) {
+            res.send(matchingClassifications);
+            console.log(matchingClassifications);
+            return;
+          }
+        }
+      }
     }
   }
-  if (type) {
-    res.send(type);
-    console.log(type);
-  } else {
-    let err = "Item not found!";
-    res.status(404);
-    res.send(err);
-    console.error(err);
-  }
 
-  res.send(category);
+  let err = "Item not found!";
+  res.status(404);
+  res.send(err);
+  console.error(err);
 });
-
-
-
 app.listen(3001);
-console.log(`Server is running on port ${3001}`);
